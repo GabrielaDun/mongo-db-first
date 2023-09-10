71cb1ce92f65a527c6698417a01af27d9a554595
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-09-10 11:35:55 +0200

    Changed departments routes to take info from MongoDB

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./../db');
+const { ObjectId } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/departments', (req, res) => {
-  res.json(db.departments);
+  req.db.collection('departments').find().toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data);
+  });
 });
 
 router.get('/departments/random', (req, res) => {
@@ -11,24 +16,35 @@ router.get('/departments/random', (req, res) => {
 });
 
 router.get('/departments/:id', (req, res) => {
-  res.json(db.departments.find(item => item.id == req.params.id));
+  req.db.collection('departments').findOne({_id: ObjectId(req.params.id)}), (err, data) => {
+    if(err) res.status(500).json({ message: err })
+    else if (!data) res.status(404).json({ message: 'Not found' })
+    else res.json(data)
+  }
 });
 
 router.post('/departments', (req, res) => {
   const { name } = req.body;
-  db.departments.push({ id: 3, name })
-  res.json({ message: 'OK' });
+  req.db.collection('departments').insertOne({name: name}), err => {
+    if(err) res.status(500).json({message: err})
+    else res.json({ message: 'OK' });
+  }
 });
 
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
+  req.db.collection('departments').updateOne({_id: ObjectId(req.params.id ) }, {$set: {name: name}}), err => {
+    if(err) res.status(500).json({ message: err})
+    else res.json({ message: 'OK' });
+  }
   db = db.departments.map(item => (item.id == req.params.id) ? { ...item, name } : item );
-  res.json({ message: 'OK' });
 });
 
 router.delete('/departments/:id', (req, res) => {
-  db = db.departments.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('departments').deleteOne({_id: ObjectId(req.params.id)}), err => {
+    if (err) res.status(500).json({ message: err })
+    else res.json({ message: 'OK' });
+  }
 });
 
 module.exports = router;
